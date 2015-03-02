d5d9b9690ff17b46475591f8b29d068dbef3aa3b
shyakaster <shyakaster@gmail.com>
2015-03-02 15:41:56 +0300

    Depot Scaffold

---
+++ db/migrate/20150302105712_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration
+  def up
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :image_url
+      t.decimal :price, :precision => 8, :scale => 2
+
+      t.timestamps null: false
+    end
+  end
+  def down
+  	drop_table :products
+  end
+end
